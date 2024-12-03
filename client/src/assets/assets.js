4254189d335348b4fc3f15aa799a4ab75812d7f2
kavetech2023 <kavetech@gmail.com>
2024-12-03 22:00:12 +0300

    initialize project

---
+++ client/src/assets/assets.js
@@ -0,0 +1,57 @@
+export const stepsData = [
+    {
+        title: 'Describe Your Vision',
+        description: 'Type a phrase, sentense or paragraph that describes the image you want to create.',
+
+    },
+    {
+        title:'Watch the Miracle',
+        description: 'Our AI-Powered engine will transform your text into high-quality, unique images in seconds.'
+    },
+    {
+        title: 'Download & Share',
+        description: 'Instantly download your creation or share it with the world directly from our platform.'
+    },
+];
+
+export const testimonialsData = [
+    {
+        name: 'Donald Karani',
+        role: 'Graphic Designer',
+        stars: 5,
+        text: `I've been using BigInt.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
+    },
+    {
+        name: 'John Doe',
+        role: 'Web Developer',
+        stars: 4,
+        text: `BigInt.removal has been a game changer for me. It has significantly improved my workflow and productivity.`
+    },
+    {
+        name: 'Jane Smith',
+        role: 'Photographer',
+        stars: 5,
+        text: `I can't imagine my work without BigInt.removal. It has made my job so much easier and more enjoyable.`
+    }
+];
+
+export const plans = [
+    {
+        id: 'Basic',
+        price: 10,
+        credits:100,
+        desc: 'Best for personal use.'
+    },
+    {
+        id: 'Pro',
+        price: 20,
+        credits:200,
+        desc: 'Best for professionals.'
+    },
+    {
+        id: 'Enterprise',
+        price: 50,
+        credits:500,
+        desc: 'Best for enterprise-level use.'
+    }
+]
